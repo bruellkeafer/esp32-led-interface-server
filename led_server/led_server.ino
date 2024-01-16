1b4c4c8827a0595177226c0deaf38de87b5aaf16
Tim Martins <s80691@bht-berlin.de>
2024-01-16 01:50:38 +0100

    prove of concept

--- led_server/led_server.ino
+++ led_server/led_server.ino
@@ -6,100 +6,137 @@
 #include <FastLED.h>
 #include "credentials.h"
 
-#define NUM_LEDS 30
-#define DATA_PIN 3
+#define NUM_LEDS 61
+#define DATA_PIN 5
 #define CLOCK_PIN 13
 CRGB leds[NUM_LEDS];
-
 WebServer server(80);
 
-const int led = 13;
 
 bool LedIsOn = true;
 
 void handleRoot() {
-  digitalWrite(led, 1);
   server.send(200, "text/html", R"html(
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Almost Smart Home</title>
+    <style>
+        .slider-color {
+            -webkit-appearance: none;
+            width: 100%;
+            height: 15px;
+            border-radius: 5px;
+            background: #d3d3d3;
+            outline: none;
+            opacity: 0.7;
+            -webkit-transition: opacity .15s ease-in-out;
+            transition: opacity .15s ease-in-out;
+        }
+    </style>
 </head>
 <body>
-    <button onclick="setStatusOn()">On</button>
-    <button onclick="setStatusOff()">Off</button>
-    <button onclick="postData(3)">Button 3</button>
+<button onclick="setStatusOn()">On</button>
+<button onclick="setStatusOff()">Off</button>
+<button onclick="postData(3)">Button 3</button>
+<input type="range" min="0" max="256" value="128" class="slider-color" id="red">
+<input type="range" min="0" max="256" value="128" class="slider-color" id="green">
+<input type="range" min="0" max="256" value="128" class="slider-color" id="blue">
 
-    <script>
-        function postData(number) {
-            fetch('/set', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ number: number })
-            })
+<script>
+    const setStatusOn = () => setStatus("on");
+    const setStatusOff = () => setStatus("off");
+
+
+    function setStatus(newStatus, r, g, b) {
+        fetch('/set', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({status: newStatus, red: r, green: g, blue: b})
+        })
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.error('Error:', error));
-        }
+    }
 
-        const setStatusOn = () => setStatus("on");
-        const setStatusOff = () => setStatus("off");
+    var r = 0, g = 0, b = 0;
+    var sliderR = document.getElementById("red");
+    var sliderG = document.getElementById("green");
+    var sliderB = document.getElementById("blue");
 
+    sliderR.oninput = function (e) {
+        r = e.target.value;
+        console.log(r);
+        setStatus("rgb", r, g, b);
+    }
 
-        function setStatus(newStatus){
-          fetch('/set', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ status: newStatus })
-            })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
-        }
-    </script>
+    sliderG.oninput = function (e) {
+        g = e.target.value;
+        console.log(g);
+        setStatus("rgb", r, g, b);
+    }
+
+    sliderB.oninput = function (e) {
+        b = e.target.value;
+        console.log(b);
+        setStatus("rgb", r, g, b);
+    }
+
+</script>
+
 </body>
 </html>
 )html");
-  digitalWrite(led, 0);
 }
 
 void handleSet() {
-  Serial.println("hallo");  
+  Serial.println("hallo");
 
   DynamicJsonDocument jsonDoc(1024);
   deserializeJson(jsonDoc, server.arg("plain"));
 
-  // Extract the status from the JSON
   String status = jsonDoc["status"];
 
-  // Log the status to the serial console
   Serial.print("Received status: ");
   Serial.println(status);
 
   // Handle the status
   if (status == "on") {
-    // Code to turn something on
     Serial.println("on");
+    for(int i=0; i <= NUM_LEDS; i++){
+      leds[i] = CRGB::White;
+    }
+    FastLED.show();
   } else if (status == "off") {
-    // Code to turn something off
     Serial.println("off");
+    for(int i=0; i <= NUM_LEDS; i++){
+      leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+  } else if (status == "rgb") {
+    Serial.println("rgb");
+    int r = jsonDoc["red"];
+    int g = jsonDoc["green"];
+    int b = jsonDoc["blue"];
+    Serial.println(r);
+    for(int i=0; i <= NUM_LEDS; i++){
+      leds[i].red = r;
+      leds[i].green = g;
+      leds[i].blue = b;
+    }
+    FastLED.show();
   } else {
     server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"Invalid status\"}");
-    digitalWrite(led, 0);
     return;
   }
 
   server.send(200, "application/json", "{\"status\":\"ok\"}");
-  digitalWrite(led, 0);
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -112,15 +149,12 @@ void handleNotFound() {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
 }
 
 void setup(void) {
-	delay(2000);
-  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  delay(2000);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -148,19 +182,12 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  for (int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+  }
 }
 
 void loop(void) {
   server.handleClient();
-  for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[whiteLed] = CRGB::White;
-      
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();
-
-      // Turn our current led back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
-   }
   delay(2);  // allow the CPU to switch to other tasks
 }
